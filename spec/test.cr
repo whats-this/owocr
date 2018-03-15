9ea7216b97ae38d9050856413c1433da400dcbb8
Proximyst <proximyst@proximyst.com>
2018-03-15 20:27:19 +0100

    Spec stuff

--- spec/test.cr
+++ spec/test.cr
@@ -1,7 +1,10 @@
 require "../src/owocr.cr"
+require "spec"
 
-token = ENV["OWO_TOKEN"]?
-raise Exception.new "no token in ENV[OWO_TOKEN]" if token.nil?
-inst = OwO::WhatsThis.new token
-puts inst.shorten("https://duckduckgo.com")
-puts inst.upload_file(File.new "image.png").files[0].url
+describe OwO::WhatsThis do
+  token = ENV["OWO_TOKEN"]?
+  raise Exception.new "no token in ENV[OWO_TOKEN]" if token.nil?
+  inst = OwO::WhatsThis.new token
+  puts "https://owo.whats-th.is/" + inst.shorten("https://duckduckgo.com")
+  puts "https://owo.whats-th.is/" + inst.upload_file(File.new "image.png").files[0].url
+end
