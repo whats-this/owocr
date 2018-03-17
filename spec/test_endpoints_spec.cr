33b62aeeece3ea0cae75d5e69c4dc2284f9313d9
Proximyst <proximyst@proximyst.com>
2018-03-17 15:25:06 +0100

    Add more testing to comply with guidelines

--- spec/test_endpoints_spec.cr
+++ spec/test_endpoints_spec.cr
@@ -1,10 +1,37 @@
 require "../src/owocr.cr"
 require "spec"
+require "http"
+require "random"
+
+# Configuration for the spec
+check_file_data = true
+image_file_name = "image.png"
+shorten_url = "https://duckduckgo.com/"
+# End configuration
+
+random = Random.new
 
 describe OwO::WhatsThis do
   token = ENV["OWO_TOKEN"]?
   raise Exception.new "no token in ENV[OWO_TOKEN]" if token.nil?
   inst = OwO::WhatsThis.new token
-  puts "https://owo.whats-th.is/" + inst.shorten("https://duckduckgo.com")
-  puts "https://owo.whats-th.is/" + inst.upload_file(File.new "image.png").files[0].url
+  shortened_id = inst.shorten shorten_url
+  puts "https://owo.whats-th.is/" + shortened_id
+  image_id = inst.upload_file(File.new image_file_name).files[0].url
+  puts "https://owo.whats-th.is/" + image_id
+  if check_file_data
+    response = HTTP::Client.get "https://owo.whats-th.is/" + image_id
+    raise Exception.new "couldn't get https://owo.whats-th.is/" + image_id + "!" if response.status_code != 200
+    raise Exception.new "the data on each part is inequal" if response.body != File.read image_file_name
+    puts "data is equal!"
+  end
+  bytes = random.random_bytes 1024
+  bytes_id = inst.upload_data(bytes, "data.bin").files[0].url
+  puts "https://owo.whats-th.is/" + bytes_id
+  if check_file_data
+    response = HTTP::Client.get "https://owo.whats-th.is/" + bytes_id
+    raise Exception.new "couldn't get https://owo.whats-th.is/" + bytes_id + "!" if response.status_code != 200
+    raise Exception.new "the data on each part is inequal" if response.body != String.new(bytes)
+    puts "byte data is equal!"
+  end
 end
